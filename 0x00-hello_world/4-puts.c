998c5b76c55a191fee2adb035a51a802380af124
Mayiikay <mayiikayuel22@gmail.com>
2023-06-16 02:20:17 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
+#include <studio.h>
+/**
+* main - Entry - A program that prints a line with puts function 
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzl");
+return (0);
+}
+
 
