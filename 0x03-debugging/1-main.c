eaa2c148ca1bd7c0c2bf605d01bab7fd28841007
Mayiikay <mayiikayuel22@gmail.com>
2023-06-22 20:35:47 +0300

    all files

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *Main - causes an infinite loop
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	Print("Infinite loop incoming :(\n");
+	i = 0;
+	/*while (i < 10)*/
+	/*{*/
+	/*   Putchar(i);*/
+	/*}*/
+	Printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
+
+Qn: 2
+
+
+
