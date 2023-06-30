033cc735d4d16ed42adc2391ccc627ca92b25f55
Mayiikay <mayiikayuel22@gmail.com>
2023-06-30 19:50:28 +0300

    4 files

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,29 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * rot13 - encoder rot13
- * @s: pointer to strong params
- *
- * Return: *s
+ * rot13 - encodes a string using rot13.
+ * @s: int type array pointer
+ * Return: encoded
  */
+
 char *rot13(char *s)
 {
-	 int i;
-	 int j;
+	int i, ii;
+	char input[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	 char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	 char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	 for (i = 0; s[i] != '\0'; i++)
-	 {
-		 for (j = 0; j < 52; j++)
-		 {
-			 if (s[i] == data1[j])
-			 {
-				 s[i] = datarot[j];
-				 break;
-			 }
-		 }
-	 }
-	 return (s);
- }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (ii = 0; ii < 54; ii++)
+		{
+			if (((s[i] <= 'z' && s[i] >= 'a') || (s[i] <= 'Z' && s[i] >= 'A'))
+					&& s[i] == input[ii])
+			{
+				s[i] = output[ii];
+				break;
+			}
+		}
+	}
+	return (s);
+}
