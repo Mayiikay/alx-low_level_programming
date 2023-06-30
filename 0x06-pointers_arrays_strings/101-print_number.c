f92fd88149596cd6dad66750e26bee9619b7b1ab
Mayiikay <mayiikayuel22@gmail.com>
2023-06-30 04:01:58 +0300

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+	if (n < 0)
+	{
+		_putchar('-');
+	}
+}
+
